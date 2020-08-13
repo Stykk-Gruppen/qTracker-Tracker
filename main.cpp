9a8406b21adb6bbccc5227100d62777137792df0
Tarald <taraldv@gmail.com>
2020-08-13 09:31:25 +0200

    added doc intro

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 #include "Database.h"
 
 using namespace bencode;
-
+/*! \mainpage QTracker-Tracker Documentation
+ *
+ * \section intro_sec Introduction
+ *
+* This document describes all the C++ classes used in our QTracker-Tracker.<br>
+ * There are no ads on this page, but due to the unforunate name of the software,
+ * adblockers will disable some functionality. Disable it to view the webpage as
+ * intended.
+ *
+ */
 int main()
 {
 	//Database *d = new Database();
